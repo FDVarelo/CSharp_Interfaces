9d8e7efaaf7cacba51b47c03cd41f70d78debf2f
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-14 08:17:46 -0300

    Problema do diamante

---
+++ ConsoleApp1/ConsoleApp4/Devices/ComboDevice.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsoleApp4.Devices
+{
+    class ComboDevice : Scanner, Printer // Impossível passar os dois ao mesmo tempo multiplas superclasses, previnindo assim o problema do diamante.
+    {
+    }
+}
