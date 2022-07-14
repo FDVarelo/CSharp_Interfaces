bef9d48ae91262c882b06d978b2ff2b2a8d9f706
Fred Daniel Varelo <fdvarelo@gmail.com>
2022-07-14 10:20:50 -0300

    Iniciando problema

--- ConsoleApp1/ConsoleApp5/Program.cs
+++ ConsoleApp1/ConsoleApp5/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\CSharp_Interfaces\ConsoleApp1\ConsoleApp5\texts\file1.txt";
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path)){
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
